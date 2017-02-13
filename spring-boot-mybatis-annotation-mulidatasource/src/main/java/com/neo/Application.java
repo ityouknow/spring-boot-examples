d4588da0329a6fd54052ffc96a0c2c58899c39bf
ityouknow <ityouknow@126.com>
2017-02-13 15:56:26 +0800

    add

--- spring-boot-mybatis-annotation-mulidatasource/src/main/java/com/neo/Application.java
+++ spring-boot-mybatis-annotation-mulidatasource/src/main/java/com/neo/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-@MapperScan("com.neo.mapper")
 public class Application {
 
 	public static void main(String[] args) {
