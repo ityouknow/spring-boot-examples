920064ea24185060d3b5543ecec4ea7257fee2ad
lizhipeng <lizhipeng@gmail.com>
2018-01-02 16:21:16 +0800

    spring boot web ok

--- spring-boot-web/src/test/java/com/neo/web/ProPertiesTest.java
+++ spring-boot-web/src/test/java/com/neo/web/ProPertiesTest.java
@@ -1,40 +1,36 @@
 package com.neo.web;
 
-import java.util.HashMap;
 import java.util.Map;
-
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
-
-import com.neo.Application;
-import com.neo.util.NeoProperties;
 import org.springframework.test.context.junit4.SpringRunner;
+import com.google.common.collect.Maps;
+import com.neo.util.NeoProperties;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ProPertiesTest {
 
-	
-	@Autowired
-    private NeoProperties neoProperties;
+
+  @Autowired
+  private NeoProperties neoProperties;
 
 
-    @Test
-    public void getHello() throws Exception {
-    	System.out.println(neoProperties.getTitle());
-        Assert.assertEquals(neoProperties.getTitle(), "纯洁的微笑");
-        Assert.assertEquals(neoProperties.getDescription(), "分享生活和技术");
-    }
+  @Test
+  public void getHello() throws Exception {
+    Assert.assertEquals("欢迎", neoProperties.getTitle());
+    Assert.assertEquals("Welcome 李志鹏", neoProperties.getDescription());
+  }
 
-    
-    @Test
-    public void testMap() throws Exception {
-    	Map<String, Long> orderMinTime=new HashMap<String, Long>();
-    	long xx=orderMinTime.get("123");
-    }
+
+  @Test
+  public void testMap() throws Exception {
+    Map<String, Long> orderMinTime = Maps.newLinkedHashMap();
+    Long xx = orderMinTime.get("123");
+    System.out.println("xx=" + xx);
+  }
 
 }
