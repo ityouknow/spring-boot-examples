75d16296b7c2184b3a72b3ece3a3421d8c5d562c
正义之剑 <yuhaibao324@126.com>
2018-03-04 00:19:34 +0800

    单元测试

--- schedule-taskplan/src/test/java/com/tools/schedulejob/web/TaskwebControlerTests.java
+++ schedule-taskplan/src/test/java/com/tools/schedulejob/web/TaskwebControlerTests.java
@@ -9,10 +9,7 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.http.MediaType;
-import org.springframework.mock.web.MockServletContext;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.test.context.web.WebAppConfiguration;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
@@ -21,9 +18,6 @@ import org.springframework.test.web.servlet.setup.MockMvcBuilders;
 
 import com.tools.schedulejob.web.TaskwebController;
 
-import static org.hamcrest.Matchers.equalTo;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.content;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
