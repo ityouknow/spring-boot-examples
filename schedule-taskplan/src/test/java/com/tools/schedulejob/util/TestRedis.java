e0f4183727cf5c093eddd20ee68f3bc24cf4040c
正义之剑 <yuhaibao324@126.com>
2018-03-04 11:07:55 +0800

    测试类修改

--- schedule-taskplan/src/test/java/com/tools/schedulejob/util/TestRedis.java
+++ schedule-taskplan/src/test/java/com/tools/schedulejob/util/TestRedis.java
@@ -12,9 +12,10 @@ import org.springframework.data.redis.core.StringRedisTemplate;
 import org.springframework.data.redis.core.ValueOperations;
 
 
-import com.tools.schedulejob.domain.User;
 
 import org.springframework.test.context.junit4.SpringRunner;
+
+import com.tools.schedulejob.domain.User;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -23,15 +24,18 @@ public class TestRedis {
     @Autowired
     private StringRedisTemplate stringRedisTemplate;
     
+	@SuppressWarnings("rawtypes")
 	@Autowired
-	private RedisTemplate<String, User> redisTemplate;
+	private RedisTemplate redisTemplate;
 
+	
     @Test
     public void test() throws Exception {
         stringRedisTemplate.opsForValue().set("aaa", "111");
         Assert.assertEquals("111", stringRedisTemplate.opsForValue().get("aaa"));
     }
     
+
     @Test
     public void testObj() throws Exception {
         User user=new User("aa@126.com", "aa", "aa123456", "aa","123");
