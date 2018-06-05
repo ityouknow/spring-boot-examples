f3a782d0772aaa5021d997d03de39a9eb1b8c9f7
Clear Cheng <360284453@qq.com>
2018-06-06 01:35:23 +0800

    Update User.java

--- spring-boot-jpa-thymeleaf-curd/src/main/java/com/neo/entity/User.java
+++ spring-boot-jpa-thymeleaf-curd/src/main/java/com/neo/entity/User.java
@@ -1,14 +1,21 @@
 package com.neo.entity;
 
+import java.sql.Date;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
-
+/**
+ * @GeneratedValue(strategy = GenerationType.IDENTITY) 采用数据库自增方式，oracle不支持此种方式
+ * @GeneratedValue(strategy = GenerationType.SEQUENCE) 采用数据库sequence序列机制生成主键。mysql不支持此种方式。
+ * @GeneratedValue(strategy = GenerationType.AUTO)     主键增长方式由数据库自动选择，使用表存储生成的主键，可以跨数据库
+ *
+ */
 @Entity
+@Table(name="user")
 public class User {
     @Id
-    @GeneratedValue
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private long id;
     @Column(nullable = false, unique = true)
     private String userName;
@@ -16,6 +23,8 @@ public class User {
     private String password;
     @Column(nullable = false)
     private int age;
+    
+	private Date birthDay;
 
     public long getId() {
         return id;
@@ -52,4 +61,12 @@ public class User {
         this.age = age;
         return this;
     }
+    
+    public Date getBirthDay() {
+		return birthDay;
+	}
+
+	public void setBirthDay(Date birthDay) {
+		this.birthDay = birthDay;
+	}
 }
