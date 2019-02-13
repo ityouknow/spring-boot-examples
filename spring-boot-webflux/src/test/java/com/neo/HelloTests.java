3a45667fea6a744885786bcfba5e1e8d94cd741b
纯洁的微笑 <ityouknow@126.com>
2019-02-13 11:24:38 +0800

    add spring-boot-webflux

---
+++ spring-boot-webflux/src/test/java/com/neo/HelloTests.java
@@ -0,0 +1,21 @@
+package com.neo;
+
+import com.neo.web.HelloController;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+
+@RunWith(SpringRunner.class)
+@WebFluxTest(controllers = HelloController.class)
+public class HelloTests {
+    @Autowired
+    WebTestClient client;
+
+    @Test
+    public void getHello() {
+        client.get().uri("/hello").exchange().expectStatus().isOk();
+    }
+}
