831f3f29352fa7b11dd88e29130611bdb6035679
gehx <1731571327@qq.com>
2019-10-12 13:14:53 +0800

    ghxtest

--- spring-boot-redis/src/main/java/com/neo/RedisApplication.java
+++ spring-boot-redis/src/main/java/com/neo/RedisApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class RedisApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(RedisApplication.class, args);
 	}
