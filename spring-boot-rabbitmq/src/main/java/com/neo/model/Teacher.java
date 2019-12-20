74d34aa3b924a26151f885ae4feae8597b17ebbd
gehx <1731571327@qq.com>
2019-12-20 14:58:36 +0800

    fgh

---
+++ spring-boot-rabbitmq/src/main/java/com/neo/model/Teacher.java
@@ -0,0 +1,13 @@
+package com.neo.model;
+
+public class Teacher {
+    private String name;
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+}
