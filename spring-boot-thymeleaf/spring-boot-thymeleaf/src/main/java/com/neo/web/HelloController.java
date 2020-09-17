c5ce517bbe56a913a83e135e29d4cc0a29b3ea76
black <wyb@email.com>
2020-09-17 22:50:42 +0800

    teat1

--- spring-boot-thymeleaf/spring-boot-thymeleaf/src/main/java/com/neo/web/HelloController.java
+++ spring-boot-thymeleaf/spring-boot-thymeleaf/src/main/java/com/neo/web/HelloController.java
@@ -7,7 +7,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 public class HelloController {
 
-    @RequestMapping("/")
+    @RequestMapping("/hello")
     public String index(ModelMap map) {
         map.addAttribute("message", "http://www.ityouknow.com");
         return "hello";
