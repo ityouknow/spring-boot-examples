0d05612c52004ac74aac80525cc0445fa99c15cd
Ishmeet Singh <76062864+IshmeetSingh06@users.noreply.github.com>
2022-09-15 23:09:25 +0530

    Added Lombok Annotations
    Removed the Boiler Code using Lombok dependency

--- spring-boot-web/src/main/java/com/neo/model/User.java
+++ spring-boot-web/src/main/java/com/neo/model/User.java
@@ -8,6 +8,10 @@ import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 
 @Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 public class User implements Serializable {
 
 	private static final long serialVersionUID = 1L;
@@ -24,53 +28,5 @@ public class User implements Serializable {
 	private String nickName;
 	@Column(nullable = false)
 	private String regTime;
-
-	public User() {
-		super();
-	}
-	public User(String nickName,String email,String userName,  String passWord, String regTime) {
-		super();
-		this.email = email;
-		this.nickName = nickName;
-		this.passWord = passWord;
-		this.userName = userName;
-		this.regTime = regTime;
-	}
-	public Long getId() {
-		return id;
-	}
-	public void setId(Long id) {
-		this.id = id;
-	}
-	public String getUserName() {
-		return userName;
-	}
-	public void setUserName(String userName) {
-		this.userName = userName;
-	}
-	public String getPassWord() {
-		return passWord;
-	}
-	public void setPassWord(String passWord) {
-		this.passWord = passWord;
-	}
-	public String getEmail() {
-		return email;
-	}
-	public void setEmail(String email) {
-		this.email = email;
-	}
-	public String getNickName() {
-		return nickName;
-	}
-	public void setNickName(String nickName) {
-		this.nickName = nickName;
-	}
-	public String getRegTime() {
-		return regTime;
-	}
-	public void setRegTime(String regTime) {
-		this.regTime = regTime;
-	}
 
 }
