a6a2a1e74f4635d057d466daae1edfb31402fc30
ityouknow <ityouknow@126.com>
2022-12-20 11:52:15 +0800

    add spring-boot-banner

---
+++ 3.x/spring-boot-banner/src/main/java/com/neo/banner/BannerApplication.java
@@ -0,0 +1,12 @@
+package com.neo.banner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BannerApplication.class, args);
+	}
+}
