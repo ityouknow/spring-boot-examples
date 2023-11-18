6565ea94d29ddabb325315af36d99ae029127c96
kukabo <102449619+kukabo@users.noreply.github.com>
2023-11-19 00:29:25 +0800

    Delete 1.x directory

--- 1.x/spring-boot-shiro/src/main/java/com/neo/dao/UserInfoDao.java
+++
@@ -1,9 +0,0 @@
-package com.neo.dao;
-
-import com.neo.entity.UserInfo;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserInfoDao extends CrudRepository<UserInfo,Long> {
-    /**通过username查找用户信息;*/
-    public UserInfo findByUsername(String username);
-}
--- 1.x/spring-boot-shiro/src/main/java/com/neo/sevice/UserInfoService.java
+++
@@ -1,8 +0,0 @@
-package com.neo.sevice;
-
-import com.neo.entity.UserInfo;
-
-public interface UserInfoService {
-    /**通过username查找用户信息;*/
-    public UserInfo findByUsername(String username);
-}
